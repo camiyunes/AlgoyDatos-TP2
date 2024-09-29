40f773b722e7de2dbfac2412aee5e7c0d5780a22
camiyunes <cyunes@fi.uba.ar>
2024-09-29 17:00:23 -0300

    inicio implementación de cartas

--- Carta.java
+++ Carta.java
@@ -21,6 +21,38 @@ public class Carta {
 		this.proximo = prox;
 	}
 	
-	
+	public String getCarta() {
+        return this.jugada;
+    }
+
+    public void jugarCarta() {
+        switch (this.jugada) {
+            case "Perder el turno":
+                System.out.println("El jugador pierde su turno.");
+                break;
+            case "Bloquear ficha":
+                System.out.println("Se bloquea la ficha de otro jugador.");
+                // bloquear una ficha de otro jugador
+                break;
+            case "Anular casillero":
+                System.out.println("Se anula un casillero del tablero.");
+                // anular un casillero del tablero
+                break;
+            case "Volver atrás jugada":
+                System.out.println("Se vuelve atrás una jugada del turno.");
+                // volver atrás una jugada
+                break;
+            case "Cambiar color ficha":
+                System.out.println("Se cambia el color de una ficha.");
+                // cambiar el color de una ficha
+                break;
+            case "Intercambiar fichas":
+                System.out.println("Se intercambian dos fichas.");
+                // intercambiar dos fichas
+                break;
+            default:
+                System.out.println("Jugada desconocida.");
+        }
+    }
 	
 }
