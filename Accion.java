a0b0ea045a423693af155ec8a2a853d415e5d1b6
Alesio2 <alesio.cieri@gmail.com>
2024-11-14 16:37:21 -0300

    Si estás leyendo esto, mi prueba funcionó

---
+++ Accion.java
@@ -0,0 +1,5 @@
+package ar.uba.fi.cb100_onto_TP2;
+
+public enum Accion {
+	PierdeElTurno, BloquearFicha, AnulaCasilleros, DeshacerJugada, CambioColor; 
+}
---
+++ Jugada.java
@@ -0,0 +1,8 @@
+package ar.uba.fi.cb100_onto_TP2;
+
+public class Jugada {
+	private Jugador usuario;
+	private Carta carta;
+	
+	public Jugada() {}
+}
